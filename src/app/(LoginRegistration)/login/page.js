a276c44507f43f1f77a73eb4d76a021c9de318bb
SajeedAninda <sajaninda@gmail.com>
2024-02-19 15:54:24 +0600

    Designed Register Page

--- src/app/(LoginRegistration)/login/page.js
+++ src/app/(LoginRegistration)/login/page.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 import logo from "@/assets/logo/logo.png"
 import GoogleLogin from '@/components/GoogleLogin/GoogleLogin';
 import loginImg from "@/assets/login/loginImg.jpg"
@@ -14,32 +13,32 @@ const Login = () => {
                         <Image src={logo} alt='logo'></Image>
                     </div>
                     <div className='mt-8'>
-                        <h2 className='text-3xl font-bold'>Welcome Back !</h2>
-                        <p className='mt-3'>Please Login as Artisan or User to access all the available features</p>
+                        <h2 className='text-3xl text-[#442b20] font-bold'>Welcome Back !</h2>
+                        <p className='mt-3 text-[#442b20]'>Please Login as Artisan or User to access all the available features</p>
                     </div>
 
                     <form className='mt-6'>
                         <div>
                             <label className='text-md font-medium'>Email<span className='text-red-500'>*</span></label> <br></br>
-                            <input type='email' name="email" className='w-full mt-2 border-2 border-[#926d5c] px-6 py-2 rounded-md' placeholder='Enter Your Email Address'></input>
+                            <input type='email' name="email" className='w-full mt-2 border-2 border-[#442b20] px-6 py-2 rounded-md' placeholder='Enter Your Email Address'></input>
                         </div>
 
                         <div className='mt-4'>
                             <label className='text-md font-medium'>Password<span className='text-red-500'>*</span></label> <br></br>
-                            <input type='password' name="password" className='w-full mt-2 border-2 border-[#926d5c] px-6 py-2 rounded-md' placeholder='Enter Password'></input>
+                            <input type='password' name="password" className='w-full mt-2 border-2 border-[#442b20] px-6 py-2 rounded-md' placeholder='Enter Password'></input>
                         </div>
 
-                        <button type='submit' className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#926d5c] group px-6 py-2 mt-5 w-full rounded-md">
-                            <span className="relative z-10 text-[#926d5c] group-hover:text-white text-lg duration-500">Log In</span>
-                            <span className="absolute w-full h-full bg-[#926d5c] -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
-                            <span className="absolute w-full h-full bg-[#926d5c] -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                        <button type='submit' className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 mt-5 w-full rounded-md">
+                            <span className="relative z-10 text-[#442b20] group-hover:text-white text-lg duration-500">Log In</span>
+                            <span className="absolute w-full h-full bg-[#442b20] -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                            <span className="absolute w-full h-full bg-[#442b20] -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
                         </button>
                     </form>
 
-                    <p className='text-center mt-6'>Or Login With</p>
+                    <p className='text-center mt-6 text-[#442b20]'>Or Login With</p>
                     <GoogleLogin></GoogleLogin>
 
-                    <p className='text-center mt-6'>Don't have any Account? <Link className='text-[#926d5c] font-bold hover:underline' href={"/register"}>Register</Link></p>
+                    <p className='text-center mt-6 text-[#442b20]'>Don't have any Account? <Link className='text-[#442b20] font-bold hover:underline' href={"/register"}>Register</Link></p>
                 </div>
 
                 <div className='flex-1'>
--- src/app/(LoginRegistration)/register/page.js
+++ src/app/(LoginRegistration)/register/page.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import { IoPeopleSharp } from 'react-icons/io5';
+import { GiStakeHammer } from "react-icons/gi";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import Link from 'next/link';
+
 
 const Register = () => {
     return (
-        <div>
-            This is Register Page
+        <div className='bg-[#926d5c67] h-screen py-16'>
+            <div className='w-[85%] mx-auto shadow-2xl bg-white h-[95%]'>
+                <h1 className='text-[#442b20] text-3xl pt-8 font-bold text-center'>Please Select Your Role</h1>
+                <div className='flex justify-center mt-12 gap-6 items-center'>
+                    <div className='flex flex-col justify-center items-center space-y-3 hover:bg-[#442b20] rounded-xl cursor-pointer group p-6 transition-all text-center border-2 border-[#442b20] shadow-lg'>
+                        <IoPeopleSharp className='text-[#442b20] font-bold text-[140px] group-hover:text-white' />
+                        <h1 className='text-[#442b20] font-bold text-4xl italic group-hover:text-white'>Register as an User</h1>
+                    </div>
+
+                    <div className='flex flex-col justify-center items-center space-y-3 hover:bg-[#442b20] rounded-xl cursor-pointer group p-6 transition-all text-center border-2 border-[#442b20] shadow-lg'>
+                        <GiStakeHammer className='text-[#442b20] font-bold text-[140px] group-hover:text-white' />
+                        <h1 className='text-[#442b20] font-bold text-4xl italic group-hover:text-white'>Register as an Artisan</h1>
+                    </div>
+                </div>
+
+                <div className='flex justify-center mt-6'>
+                    <Link href={"/"} className=' mt-3 cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 rounded-md'>
+                        <span className="flex justify-between items-center gap-2 relative z-10 text-[#442b20] group-hover:text-white text-lg duration-500">
+                            <IoMdArrowRoundBack />
+                            Back To Home
+                        </span>
+                        <span className="absolute w-full h-full bg-[#442b20] -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                        <span className="absolute w-full h-full bg-[#442b20] -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
