d84f40a520d2daa76d19ff1d7020bd5427dd4991
SajeedAninda <sajaninda@gmail.com>
2024-02-19 22:05:40 +0600

    Implemented google login

--- src/components/GoogleLogin/GoogleLogin.jsx
+++ src/components/GoogleLogin/GoogleLogin.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+"use client"
+import useAuth from '@/Hooks/useAuth';
+import useAxiosInstance from '@/Hooks/useAxiosInstance';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 import { FaGoogle } from "react-icons/fa";
 
-
 const GoogleLogin = () => {
+    let router = useRouter();
+    let { googleLogin } = useAuth();
+    let axiosInstance = useAxiosInstance();
+
+    let handleGoogleLogin = () => {
+        googleLogin()
+            .then((result) => {
+                const user = result.user;
+                let userDetails = { name: user?.displayName, email: user?.email, imageUrl: user?.photoURL, role: "user" }
+
+                axiosInstance.post("/userGoogleRegister", userDetails)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.insertedId) {
+                            console.log(res.data);
+                        }
+                    })
+                toast.success('Logged In Successfully!', {
+                    duration: 3000,
+                });
+                router.push("/")
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
-        <div className='flex justify-center'>
+        <div onClick={handleGoogleLogin} className='flex justify-center'>
             <button className=' mt-3 cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 rounded-md'>
                 <span className="flex justify-between items-center gap-2 relative z-10 text-[#442b20] group-hover:text-white text-lg duration-500">
                     <FaGoogle />
