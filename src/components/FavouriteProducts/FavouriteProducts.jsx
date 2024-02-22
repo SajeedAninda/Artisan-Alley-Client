4efd6646f543c2aa7e6f05d0b892f4dcaefccfd8
SajeedAninda <sajaninda@gmail.com>
2024-02-22 23:21:07 +0600

    Implemented favourites delettion

--- src/components/FavouriteProducts/FavouriteProducts.jsx
+++ src/components/FavouriteProducts/FavouriteProducts.jsx
@@ -5,6 +5,9 @@ import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import toast from 'react-hot-toast';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 
 const FavouriteProducts = () => {
     let axiosInstance = useAxiosInstance();
@@ -19,7 +22,32 @@ const FavouriteProducts = () => {
         }
     })
 
-    console.log(favouriteProducts)
+    let handleDeleteFavourites = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Once Deleted, you cannot revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#926d5c',
+            cancelButtonColor: '#442b20',
+            confirmButtonText: 'Yes, Delete!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosInstance.delete(`/deleteFavourites/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            console.log(res.data);
+                            toast.success("Favourite Product Deletion Is Successful")
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error :", error);
+                        toast.error('Error', 'Failed to delete Favourite');
+                    });
+            }
+        });
+    }
 
     return (
         <div>
@@ -38,6 +66,11 @@ const FavouriteProducts = () => {
                                     <div className="bg-white rounded-lg shadow-lg flex flex-col">
                                         <div className="relative">
                                             <Image width={400} height={400} className="rounded-t-lg w-full h-[200px] object-cover" src={product?.imageUrl} alt="" />
+                                            <div className="absolute top-2 right-2">
+                                                <div onClick={() => handleDeleteFavourites(product?._id)} className="p-3 rounded-full bg-red-500 cursor-pointer">
+                                                    <RiDeleteBin5Fill className="text-white font-bold text-3xl" />
+                                                </div>
+                                            </div>
                                         </div>
                                         <div className="p-5 flex flex-col flex-grow bg-gradient-to-r from-[#442b20] to-[#926d5c] rounded-b-lg">
                                             <h5 className="mb-2 text-xl font-bold tracking-tight text-white">{product?.product_name}</h5>
