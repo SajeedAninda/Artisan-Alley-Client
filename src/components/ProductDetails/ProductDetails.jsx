0401ba0dc23176884ba152b2e57c7974408bc64e
SajeedAninda <sajaninda@gmail.com>
2024-02-22 17:21:22 +0600

    Added Product Details

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -16,16 +16,16 @@ const ProductDetails = ({ params }) => {
         enabled: !!params,
     });
 
-    let _id, product_name, product_price, product_category, product_short_description, product_broad_description, product_location, imageUrl, addedTime;
+    let _id, product_name, product_price, product_category, product_short_description, product_broad_description, product_location, imageUrl, addedTime, artisan_name, artisan_email;
     if (productDetails) {
-        ({ _id, product_name, product_price, product_category, product_short_description, product_broad_description, product_location, imageUrl, addedTime } = productDetails);
+        ({ _id, product_name, product_price, product_category, product_short_description, product_broad_description, product_location, imageUrl, addedTime, artisan_name, artisan_email } = productDetails);
     }
 
     return (
         <div className='py-8 w-[90%] mx-auto'>
-            <div className='flex gap-10 items-center'>
-                <div>
-                    <Image width={400} height={400} className="rounded-lg w-[500px] h-[400px] border-8 border-[#442b20] object-cover" src={imageUrl} alt="" />
+            <div className='flex gap-10'>
+                <div className='flex-1'>
+                    <Image width={400} height={400} className="rounded-lg w-full h-[400px] border-8 border-[#442b20] object-cover" src={imageUrl} alt="" />
 
                     <div className='space-y-3 mt-3'>
                         <button className="lg:inline-block cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 w-full">
@@ -42,8 +42,20 @@ const ProductDetails = ({ params }) => {
                     </div>
                 </div>
 
-                <div>
-
+                <div className='flex-1'>
+                    <div>
+                        <h2 className='text-4xl font-bold text-[#442b20]'>{product_name}</h2>
+                        <h2 className='text-2xl font-bold text-[#442b20] mt-3'>{product_short_description}</h2>
+                        <h2 className='text-lg font-bold text-[#442b20] mt-3'>{product_broad_description}</h2>
+                        <h2 className='text-3xl font-bold text-[#442b20] mt-3'>Product Price: {product_price}$ / Piece</h2>
+                        <h2 className='text-2xl font-bold text-[#442b20] mt-3'>Product Location: {product_location}</h2>
+                        <h2 className='text-lg font-bold text-[#442b20] mt-3'>Product ID:  {_id}</h2>
+                        <h2 className='text-lg font-bold text-[#442b20] mt-3'>Product Added on {addedTime}</h2>
+                        <div className='bg-gradient-to-r from-[#442b20] to-[#926d5c] rounded-md p-4 w-full text-center mt-3'>
+                            <h2 className='text-xl font-bold text-white'>Crafted By {artisan_name}</h2>
+                            <h2 className='text-lg font-bold text-white'>{artisan_email}</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
