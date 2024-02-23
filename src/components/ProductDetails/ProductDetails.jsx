f10d51bf03eb840dcd7cc334d04109c2e067fa14
SajeedAninda <sajaninda@gmail.com>
2024-02-23 14:53:07 +0600

    Implemented make order

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -3,13 +3,15 @@ import useAuth from '@/Hooks/useAuth';
 import useAxiosInstance from '@/Hooks/useAxiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
-import React from 'react';
+import { Fragment, useState } from 'react'
 import toast from 'react-hot-toast';
+import { Dialog, Transition } from '@headlessui/react'
 
 const ProductDetails = ({ params }) => {
     let axiosInstance = useAxiosInstance();
     let { loggedInUser } = useAuth();
     let currentUserEmail = loggedInUser?.email;
+    let [isOpen, setIsOpen] = useState(false);
 
     const { data: productDetails, isLoading: isProductLoading } = useQuery({
         queryKey: ['productDetails', params],
@@ -58,6 +60,44 @@ const ProductDetails = ({ params }) => {
             });
     }
 
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    let handleOrderProduct = (e) => {
+        e.preventDefault();
+        let client_name = loggedInUser?.displayName;
+        let client_email = loggedInUser?.email;
+        let client_phone = e.target.client_phone_number.value;
+        let shipping_address = e.target.client_shipping_address.value;
+        let order_quantity = e.target.quantity.value;
+
+        if (order_quantity <= 0 || isNaN(order_quantity)) {
+            toast.error("Invalid order quantity");
+            return;
+        }
+
+        let orderDetails = { client_name, client_email, client_phone, shipping_address, order_quantity, productId, product_name, product_price, product_category, product_short_description, product_broad_description, product_location, imageUrl, addedTime, artisan_name, artisan_email };
+        
+        let loadingToast = toast.loading('Confirming Order...');
+        axiosInstance.post(`/addOrder`, orderDetails)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.insertedId) {
+                    toast.dismiss(loadingToast);
+                    toast.success("Order Confirmed");
+                }
+            })
+            .catch(error => {
+                toast.dismiss(loadingToast);
+                console.error("Error making order:", error);
+            });
+    }
+
     return (
         <div className='py-8 w-[90%] mx-auto'>
             <div className='flex gap-10'>
@@ -65,7 +105,7 @@ const ProductDetails = ({ params }) => {
                     <Image width={400} height={400} className="rounded-lg w-full h-[400px] border-8 border-[#442b20] object-cover" src={imageUrl} alt="" />
 
                     <div className='space-y-3 mt-3'>
-                        <button className={`lg:inline-block cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 w-full ${isCurrentUserArtisan ? 'bg-gray-400 cursor-not-allowed' : ''}`} disabled={isCurrentUserArtisan}>
+                        <button onClick={openModal} className={`lg:inline-block cursor-pointer font-semibold overflow-hidden relative z-100 border border-[#442b20] group px-6 py-2 w-full ${isCurrentUserArtisan ? 'bg-gray-400 cursor-not-allowed' : ''}`} disabled={isCurrentUserArtisan}>
                             <span className="relative z-10 text-[#442b20] group-hover:text-white text-lg duration-500">Order Now</span>
                             <span className="absolute w-full h-full bg-[#442b20] -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
                             <span className="absolute w-full h-full bg-[#442b20] -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
@@ -99,6 +139,100 @@ const ProductDetails = ({ params }) => {
                     </div>
                 </div>
             </div>
+
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/25" />
+                    </Transition.Child>
+
+                    <form onSubmit={handleOrderProduct} className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full w-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-gradient-to-r from-[#442b20] to-[#926d5c] p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-2xl space-y-3 text-center font-bold leading-6 text-white"
+                                    >
+                                        <h3>{product_name}</h3>
+                                        <h3>Price: {product_price}$ / Piece</h3>
+
+                                    </Dialog.Title>
+                                    <div className="mt-5 flex flex-row gap-3 items-center justify-center">
+                                        <p className="text-lg text-white font-bold">
+                                            Name:
+                                        </p>
+                                        <p className="text-lg text-white font-bold">
+                                            {loggedInUser?.displayName}
+                                        </p>
+                                    </div>
+
+                                    <div className="mt-3 flex flex-row gap-3 items-center justify-center">
+                                        <p className="text-lg text-white font-bold">
+                                            Email:
+                                        </p>
+                                        <p className="text-lg text-white font-bold">
+                                            {loggedInUser?.email}
+                                        </p>
+                                    </div>
+
+                                    <div className="mt-3 flex flex-row gap-3 items-center justify-center">
+                                        <p className="text-lg text-white font-bold w-[20%]">
+                                            Quantity:
+                                        </p>
+                                        <input type="number" name='quantity' className='py-2 rounded-md px-4 w-[80%]' placeholder='Quantity of Items You want to order' required />
+                                    </div>
+
+                                    <div className="mt-3 flex flex-row gap-3 items-center justify-center">
+                                        <p className="text-lg text-white font-bold w-[20%]">
+                                            Phone No:
+                                        </p>
+                                        <input name='client_phone_number' type="tel" className='py-2 rounded-md px-4 w-[80%]' placeholder='Enter Your Phone Number' required />
+                                    </div>
+
+                                    <div className="mt-3 flex flex-row gap-3 items-center justify-center">
+                                        <p className="text-lg text-white font-bold w-[20%]">
+                                            Shipping Address:
+                                        </p>
+                                        <input name='client_shipping_address' type="text" className='py-2 rounded-md px-4 w-[80%]' placeholder='Enter Your Address' required />
+                                    </div>
+
+
+                                    <div className="mt-8 flex justify-center items-center gap-6">
+                                        <button type='submit' className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-white group px-6 py-2">
+                                            <span className="relative z-10 text-white group-hover:text-[#926d5c] text-lg duration-500">Make Order</span>
+                                            <span className="absolute w-full h-full bg-white -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                                            <span className="absolute w-full h-full bg-white -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                                        </button>
+
+                                        <button type="button" onClick={closeModal} className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-white group px-6 py-2">
+                                            <span className="relative z-10 text-white group-hover:text-[#926d5c] text-lg duration-500">Cancel</span>
+                                            <span className="absolute w-full h-full bg-white -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                                            <span className="absolute w-full h-full bg-white -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </form>
+                </Dialog>
+            </Transition>
         </div>
     );
 };
